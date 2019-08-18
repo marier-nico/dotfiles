2e2072533bd2b4ef5730788cfa02bed9802dcccd
Nicolas Marier <software@nmarier.com>
2019-08-17 20:43:25 -0400

    Add keybindings for volume controls

--- config/dwm/config.def.h
+++ config/dwm/config.def.h
@@ -1,5 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
+#include <X11/XF86keysym.h>
+
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
@@ -61,41 +63,48 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "rofi", "-show", "run" };
 static const char *termcmd[]  = { "st", NULL };
 
+static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%",     NULL };
+static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "@DEFAULT_SINK@", "-5%",     NULL };
+static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "@DEFAULT_SINK@", "toggle",  NULL };
+
 static Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_z,      zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY,                       XK_w,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	/* modifier                     key        		 function        argument */
+	{ MODKEY,                       XK_p,      		 spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_Return, 		 spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_b,      		 togglebar,      {0} },
+	{ MODKEY,                       XK_j,      		 focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,      		 focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_i,      		 incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_d,      		 incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_h,      		 setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,      		 setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_z,      		 zoom,           {0} },
+	{ MODKEY,                       XK_Tab,    		 view,           {0} },
+	{ MODKEY,                       XK_w,      		 killclient,     {0} },
+	{ MODKEY,                       XK_t,      		 setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_f,      		 setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_m,      		 setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_space,  		 setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_space,  		 togglefloating, {0} },
+	{ MODKEY,                       XK_0,      		 view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,      		 tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,  		 focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period, 		 focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,  		 tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period, 		 tagmon,         {.i = +1 } },
+	TAGKEYS(                        XK_1,                      		 0)
+	TAGKEYS(                        XK_2,                      		 1)
+	TAGKEYS(                        XK_3,                      		 2)
+	TAGKEYS(                        XK_4,                      		 3)
+	TAGKEYS(                        XK_5,                      		 4)
+	TAGKEYS(                        XK_6,                      		 5)
+	TAGKEYS(                        XK_7,                      		 6)
+	TAGKEYS(                        XK_8,                      		 7)
+	TAGKEYS(                        XK_9,                      		 8)
+	{ MODKEY|ShiftMask,             XK_q,      		 quit,           {0} },
+	{ 0,                       	XF86XK_AudioLowerVolume, spawn, 	 {.v = downvol } },
+	{ 0,                       	XF86XK_AudioMute, 	 spawn, 	 {.v = mutevol } },
+	{ 0,                       	XF86XK_AudioRaiseVolume, spawn, 	 {.v = upvol   } }
 };
 
 /* button definitions */
