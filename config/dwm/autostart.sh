618033ef07531984b3cd49d1e18ec735c3c801a0
Nicolas Marier <software@nmarier.com>
2019-09-26 22:31:42 -0400

    Add wacom button mappings

--- config/dwm/autostart.sh
+++ config/dwm/autostart.sh
@@ -4,6 +4,8 @@ feh --no-fehbg --bg-scale $HOME/Pictures/*.{jpg,png}
 
 setxkbmap ca
 xset r rate  150 30
+xsetwacom --set "Wacom Intuos S 2 Pad pad" Button 1 "key +ctrl z -ctrl"
+xsetwacom --set "Wacom Intuos S 2 Pad pad" Button 3 "key +ctrl y -ctrl"
 
 dunst &
 source ~/.dwm/dwm_status.sh &
