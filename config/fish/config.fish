8988cf6fa3110d9d9008062f35c04bb653402eac
Nicolas Marier <software@nmarier.com>
2019-11-16 14:54:05 -0500

    Start virtualfish in shell config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,6 +3,9 @@
 # make gpg-agent work
 set -g -x GPG_TTY (tty)
 
+# activate virtualfish
+eval (python -m virtualfish)
+
 # install fisher if needed and download plugins
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
