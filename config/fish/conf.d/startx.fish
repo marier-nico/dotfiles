e37caebe54dc100ae9085ad778ae7885c3a528c2
Nicolas Marier <software@nmarier.com>
2021-12-08 21:22:16 -0500

    refactor: cleanup unused dotfiles

--- config/fish/conf.d/startx.fish
+++
@@ -1,3 +0,0 @@
-set TTY1 (tty)
-set -gx _JAVA_AWT_WM_NONREPARENTING 1
-[ "$TTY1" = "/dev/tty1" ] && startx
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,11 +1,7 @@
 if status is-interactive
+    /opt/homebrew/bin/brew shellenv | source
     pyenv init --path | source
-    pyenv init - | source
-
-    venv-wrapper init fish | source
-    venv completions
+    source /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
+    set PATH $PATH /Users/nmarier/.local/bin
+    set PATH $PATH /Users/nmarier/.cargo/bin
 end
-
-thefuck --alias | source
-
-source /usr/share/fish/vendor_conf.d/autojump.fish
