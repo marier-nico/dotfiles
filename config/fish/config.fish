40e2100310b08eb6046278287e6a3351e3c33c24
Nicolas Marier <software@nmarier.com>
2022-07-10 14:05:13 -0400

    feat: conditionally setup env vars with what's available

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,7 +1,14 @@
 if status is-interactive
-    /opt/homebrew/bin/brew shellenv | source
-    pyenv init --path | source
-    source /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
-    set PATH $PATH /Users/nmarier/.local/bin
-    set PATH $PATH /Users/nmarier/.cargo/bin
+    switch (uname)
+        case Darwin
+            /opt/homebrew/bin/brew shellenv | source
+            source /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
+    end
+
+    if type -q "pyenv"
+        pyenv init --path | source
+    end
+
+    set PATH $PATH $HOME/.local/bin
+    set PATH $PATH $HOME/.cargo/bin
 end
