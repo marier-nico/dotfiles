af349e8f12dd72ba9cecebeeb17960bc78c8d75f
Nicolas Marier <software@nmarier.com>
2023-07-31 13:58:51 -0400

    feat: add telescope current file fuzzy find

--- config/nvim/after/plugin/telescope.lua
+++ config/nvim/after/plugin/telescope.lua
@@ -1,6 +1,7 @@
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+vim.keymap.set("n", "gf", builtin.current_buffer_fuzzy_find, { desc = "Current buffer fuzzy find" })
 vim.keymap.set("n", "<leader>gs", builtin.git_status, { desc = "Git status" })
 vim.keymap.set("n", "gr", builtin.lsp_references, { desc = "LSP references" })
 vim.keymap.set("n", "gd", builtin.lsp_definitions, { desc = "LSP definitions" })
