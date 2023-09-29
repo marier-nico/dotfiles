6a40727ac0c7a19d46f3e50d02d3cf012b5f2d61
Nicolas Marier <software@nmarier.com>
2023-09-29 15:30:12 -0400

    feat: resize nvim-tree and better autoclose

--- config/nvim/after/plugin/nvim_tree.lua
+++ config/nvim/after/plugin/nvim_tree.lua
@@ -1,16 +1,44 @@
 local api = require("nvim-tree.api")
 
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+	on_attach = function(bufnr)
+		local function opts(desc)
+			return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+		end
+
+		-- default mappings
+		api.config.mappings.default_on_attach(bufnr)
+
+		-- custom mappings
+		vim.keymap.set("n", "L", ":NvimTreeResize +20<CR>", opts("Resize window (+20)"))
+		vim.keymap.set("n", "H", ":NvimTreeResize -20<CR>", opts("Resize window (-20)"))
+	end,
+})
+
 vim.keymap.set("n", "<leader>e", function()
 	api.tree.toggle({ find_file = true, focus = true })
 end, { desc = "Toggle file tree" })
 
--- Auto close
-vim.api.nvim_create_autocmd("BufEnter", {
-	nested = true,
+-- Magic auto close by @marvinth01
+vim.api.nvim_create_autocmd("QuitPre", {
 	callback = function()
-		if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
-			vim.cmd("quit")
+		local tree_wins = {}
+		local floating_wins = {}
+		local wins = vim.api.nvim_list_wins()
+		for _, w in ipairs(wins) do
+			local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
+			if bufname:match("NvimTree_") ~= nil then
+				table.insert(tree_wins, w)
+			end
+			if vim.api.nvim_win_get_config(w).relative ~= "" then
+				table.insert(floating_wins, w)
+			end
+		end
+		if 1 == #wins - #floating_wins - #tree_wins then
+			-- Should quit, so we close all invalid windows.
+			for _, w in ipairs(tree_wins) do
+				vim.api.nvim_win_close(w, true)
+			end
 		end
 	end,
 })
