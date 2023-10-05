272087ed12c2b0de64ca4923feebc31093045af8
Nicolas Marier <software@nmarier.com>
2023-10-05 16:11:00 -0400

    feat: relative ts imports & longer format on save timeout

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -131,6 +131,7 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 	callback = function()
 		vim.lsp.buf.format({
 			async = false,
+			timeout_ms = 30000,
 			filter = function(client)
 				return client.name ~= "tsserver"
 			end,
--- config/nvim/after/plugin/typescript_tools.lua
+++ config/nvim/after/plugin/typescript_tools.lua
@@ -1,5 +1,9 @@
 require("typescript-tools").setup({
 	settings = {
+		tsserver_file_preferences = {
+			importModuleSpecifierPreference = "relative",
+			importModuleSpecifierEnding = "minimal",
+		},
 		tsserver_plugins = {
 			"@styled/typescript-styled-plugin",
 		},
