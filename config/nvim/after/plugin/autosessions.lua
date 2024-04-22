398989f1daad3b219d19318353f4833104ac5022
Nicolas Marier <software@nmarier.com>
2024-04-22 14:58:15 -0400

    feat: add session name to lualine

--- config/nvim/after/plugin/autosessions.lua
+++ config/nvim/after/plugin/autosessions.lua
@@ -27,6 +27,18 @@ require("auto-session").setup({
 	},
 	auto_restore_enabled = false,
 	pre_save_cmds = { pre_save },
+	post_restore_cmds = {
+		require("lualine").refresh,
+		function()
+			-- Restart doesn't work when servers haven't started
+			vim.cmd("LspStop")
+			vim.cmd("LspStart")
+		end,
+		function()
+			local session_name = require("auto-session.lib").current_session_name()
+			io.popen("alacritty msg config 'window.title=\"" .. session_name .. "\"'")
+		end,
+	},
 })
 
 vim.keymap.set("n", "<C-a>", require("auto-session.session-lens").search_session, {
--- config/nvim/after/plugin/lualine.lua
+++ config/nvim/after/plugin/lualine.lua
@@ -2,4 +2,7 @@ require("lualine").setup({
 	options = {
 		theme = "catppuccin",
 	},
+	sections = {
+		lualine_y = { require("auto-session.lib").current_session_name },
+	},
 })
