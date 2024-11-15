2d12ac1f394bfe286cd0902a63cf362bd456855c
Nicolas Marier <software@nmarier.com>
2024-11-14 20:17:13 -0500

    feat: slimmer prompt when term is narrow

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,15 +5,26 @@ if status is-interactive
         pyenv init --path | source
     end
 
-    if test -e "/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc"
-        source "/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc"
-    end
-
     set -gx VOLTA_HOME "$HOME/.volta"
     set -gx PATH "$VOLTA_HOME/bin" $PATH
 
     set -g direnv_fish_mode disable_arrow
     direnv hook fish | source
 
-    starship init fish | source
+    #starship init fish | source
+end
+
+source ~/.config/fish/functions/lucid-prompt.fish
+
+# Suppress default mode prompt
+function fish_mode_prompt
+end
+
+
+function fish_prompt
+    if [ (tput cols) -le 100 ]
+        echo '❯ '
+    else
+        lucid_fish_prompt
+    end
 end
