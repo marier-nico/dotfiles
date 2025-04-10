e100221d2187029440ffbce959e4dddd67301b4d
Nicolas Marier <software@nmarier.com>
2025-04-10 17:54:43 -0400

    chore: update nvim

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,17 +1,7 @@
 if status is-interactive
     /opt/homebrew/bin/brew shellenv | source
 
-    if type -q "pyenv"
-        pyenv init --path | source
-    end
-
-    set -gx VOLTA_HOME "$HOME/.volta"
-    set -gx PATH "$VOLTA_HOME/bin" $PATH
-
-    set -g direnv_fish_mode disable_arrow
-    direnv hook fish | source
-
-    #starship init fish | source
+    fnm env --shell fish | source
 end
 
 source ~/.config/fish/functions/lucid-prompt.fish
